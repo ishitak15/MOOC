d36528577e95407ee6b2fe9d500281d3cca6b18c
Ishita Kale <1262251255@mitwpu.edu.in>
2026-04-13 19:45:16 +0530

    Add sorting functionality in week12.c
    Implement a sorting algorithm to sort an array of integers.

---
+++ week12.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void sort(int *a, int n);
+int main()
+{
+    int a[20];
+    int n,i; 
+    scanf("%d",&n); // Enter number of elements to sort is taken from test case data
+   
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]); // The elements of the array is taken from the test data
+    }
+
+sort(a, n); // Calling the sorting function
+
+    //Printing the sorted array 
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+   return 0;
+}
+void sort(int*a,int n){
+  int i,j,temp;
+  for(i=0;i<n-1;i++){
+    for(j=0;j<n-i-1;j++){
+      if (*(a+j)>*(a+j+1)){
+        temp=*(a+j);
+        *(a+j)=*(a+j+1);
+        *(a+j+1)=temp;
+      }
+    }
+  }
+}
